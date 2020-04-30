4e270d2941ac2140294902f8dcd793d7ba247d3f
Javier Torron At Sistemas <jtorron@atsistemas.com>
2020-04-30 18:05:57 +0200

    Version 2

--- weather/app.js
+++ weather/app.js
@@ -14,19 +14,27 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', function (req, res) {
+  var response = 'This is version 2 of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
